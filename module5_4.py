5ec73e86f593468fb9c26681ffcec41704218ba6
Захаров Артём Алексеевич <64315736+Artemgrachev1@users.noreply.github.com>
2025-01-03 10:21:22 +0300

    Update module5_4.py

--- module5_4.py
+++ module5_4.py
@@ -32,9 +32,11 @@ class House:
         if isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
     def __add__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors = self.number_of_floors + value
-            return self
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        elif isinstance(value, int):
+            self.number_of_floors += value
+        return self
     def __radd__(self, value):
         return self.__add__(value)
     def __iadd__(self, value):
